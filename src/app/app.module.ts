bea3747f85472bbd4e71d502305cdd5ed70f3501
Duvindu Nishad Karunasingha <Duvidunishad11@gmail.com>
2023-01-16 10:00:05 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { NxWelcomeComponent } from './nx-welcome.component';
+import { ProductListComponent } from './pages/product-list/product-list.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+
+const routes: Routes = [
+  { path: '', component: HomePageComponent },
+  { path: 'product', component: ProductListComponent },
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NxWelcomeComponent,
+    ProductListComponent,
+    HomePageComponent,
+    HeaderComponent,
+    FooterComponent,
+  ],
+  imports: [BrowserModule, RouterModule.forRoot(routes)],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
